3adee1634fa0670067454306531c052bc6d8e09d
unknown <iltfhuss@gmail.com>
2024-09-11 23:59:20 +0500

    added code

--- randomNumberGuesser.js
+++ randomNumberGuesser.js
@@ -4,14 +4,22 @@ const target = Math.rpund(Math.random() * 100);
 
 let guesses = 0;
 
-const guess = Number(prompt("Guess the number (0-100): "));
+    while(True){
+    guesses++;
 
-if (guess > target){
-    console.log("Your guess is too high.");
+    const guess = Number(prompt("Guess the number (0-100): "));
+
+    if (guess > target) {
+        console.log("Your guess is too high.");
+    }
+
+    else if (guess < target) {
+        console.log("Your guess is too low.");
+    }
+
+    else {
+        console.log("You guessed it!")
+        break;
+    }
 }
-else if(guess < target){
-    console.log("Your guess is too low.");
-}
-else{
-    console.log("You guessed it!")
-}
+console.log("You guessed", guesses, " time")
