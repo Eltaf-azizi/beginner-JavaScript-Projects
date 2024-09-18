37bf5e8f486095565a9095c71dde21935cfac859
altaf azizi <iltfhuss@gmail.com>
2024-09-18 21:49:09 +0500

    added code

---
+++ Intermidiate Javascript Projects/project.js
@@ -0,0 +1,14 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winning
+// 7. Play again
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
